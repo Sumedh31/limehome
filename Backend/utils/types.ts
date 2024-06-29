99912b390794a0bd953f451556261afe6b6b8db4
Sumedh Tambe <sumedhrtv@gmail.com>
2024-06-29 18:02:49 +0100

    map response to interface in types

---
+++ Backend/utils/types.ts
@@ -0,0 +1,71 @@
+interface Location {
+    lat: number;
+    lng: number;
+    city: string;
+    postalCode: string;
+    countryCode: string;
+    addressLine1: string;
+    countryName: string;
+  }
+  
+  interface Image {
+    url: string;
+    is_portrait: boolean;
+    position: number;
+    unit_group_ids?: number[];
+    tags?: any[];
+  }
+  
+  interface Space {
+    icon: string;
+    name: string;
+    name_plural: string;
+    slug: string;
+    value: number;
+  }
+  
+  interface Amenity {
+    icon: string;
+    name: string;
+  }
+  
+  interface UnitGroup {
+    id: number;
+    title: string;
+    description: string;
+    custom_title: string;
+    external_id: string;
+    name: string;
+    max_guests: number;
+    rental_type: string;
+    bedroom_count: number;
+    lowest_price_per_night: null | number;
+    lowest_price_per_month: null | number;
+    spaces: Space[];
+    amenities: Amenity[];
+    images: Image[];
+  }
+  
+  interface PropertyDetails {
+    id: number;
+    external_id: string;
+    review_widget_id: string;
+    name: string;
+    city: string;
+    city_id: number;
+    street: string;
+    location: Location;
+    timezone: string;
+    distance: number;
+    description: string;
+    additional_services: null | string;
+    parking: string;
+    things_to_know: string;
+    house_rules: string;
+    images: Image[];
+    lowest_price_per_night: null | number;
+    lowest_price_per_month: null | number;
+    default_check_in_time: string;
+    default_check_out_time: string;
+    unit_groups: UnitGroup[];
+  }
