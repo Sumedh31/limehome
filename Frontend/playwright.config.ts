54c6472ae112ddf0202e2479bff53ed9061a1c41
Sumedh Tambe <sumedhrtv@gmail.com>
2024-07-01 11:41:36 +0100

    Set parallel workers to 4 and increase test timeout

--- Frontend/playwright.config.ts
+++ Frontend/playwright.config.ts
@@ -3,8 +3,9 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  workers: 4,
   /* Maximum time one test can run for. */
-  timeout: 60 * 1000,
+  timeout: 90 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
